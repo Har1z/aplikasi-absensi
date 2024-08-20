c24a90a9a0ffcee7447d86c3d4a9fa2260324fff
Har1z <prdrap776@gmail.com>
2024-08-20 13:00:41 +0700

    tes

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
     session_start();
 
-    if($_SESSION['login']==true){
-        if ($_SESSION['role'] == "admin") {
-            header("location: ./admin");
-            ob_end_flush();
-            die();
-        } else if ($_SESSION['role'] == "guru") {
-            header("location: ./guru");
-            ob_end_flush();
-            die();
-        } else if ($_SESSION['role'] == "siswa") {
-            header("location: ./siswa");
-            ob_end_flush();
-            die();
-        }
-        die();
-    } else {
-        header('location: public/login');
-        die(); //seriously i better off dead rather than exit()
-    }
+    // if($_SESSION['login']==true){
+    //     if ($_SESSION['role'] == "admin") {
+    //         header("location: ./admin");
+    //         ob_end_flush();
+    //         die();
+    //     } else if ($_SESSION['role'] == "guru") {
+    //         header("location: ./guru");
+    //         ob_end_flush();
+    //         die();
+    //     } else if ($_SESSION['role'] == "siswa") {
+    //         header("location: ./siswa");
+    //         ob_end_flush();
+    //         die();
+    //     }
+    //     die();
+    // } else {
+    //     header('location: public/login.php');
+    //     die(); //seriously i better off dead rather than exit()
+    // }
+    echo "halo tris";
 ?>
